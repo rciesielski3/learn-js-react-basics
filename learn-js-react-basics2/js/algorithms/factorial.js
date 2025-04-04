02fb480c3c6626220a08fe08b6afd6935311a96e
r.ciesielski3 <r.ciesielski3@gmail.com>
2025-04-04 19:50:55 +0200

    fix: remove deep catalogue

--- learn-js-react-basics2/js/algorithms/factorial.js
+++
@@ -1,8 +0,0 @@
-export function factorial(n) {
-  if (n <= 1) return 1;
-  return n * factorial(n - 1);
-}
-// Example usage:
-// console.log(factorial(5)); // Output: 120
-// console.log(factorial(10)); // Output: 3628800
-// console.log(factorial(0)); // Output: 1
