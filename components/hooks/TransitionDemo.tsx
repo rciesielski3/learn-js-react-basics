1467c8be6579cb0c5763f018be141a6b39e90a65
r.ciesielski3 <r.ciesielski3@gmail.com>
2025-04-23 19:39:18 +0200

    feat: add examples for hook

--- components/hooks/TransitionDemo.tsx
+++ components/hooks/TransitionDemo.tsx
@@ -2,12 +2,15 @@
 
 import React from "react";
 
+import { hookData } from "@/data/hooks";
 import { ExampleBlock } from "../shared";
 
 export function TransitionDemo() {
   const [input, setInput] = React.useState("");
   const [list, setList] = React.useState<string[]>([]);
   const [isPending, startTransition] = React.useTransition();
+
+  const { sampleSnippet, visualReprezentation } = hookData.useTransition;
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -27,6 +30,8 @@ export function TransitionDemo() {
         code={`const [isPending, startTransition] = useTransition();`}
         usage={`Use startTransition for expensive updates that shouldn't block typing\nor fast feedback`}
         result={`Input length: ${input.length} | List size: ${list.length}`}
+        sampleSnippet={sampleSnippet}
+        visualReprezentation={visualReprezentation}
       />
       <div className="px-6 pb-2 -mt-3">
         <p className="text-sm text-gray-400 mb-2">
