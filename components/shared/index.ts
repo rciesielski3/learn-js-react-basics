596f94c4b58e20064f775eaef0bd8dabbdcb9403
r.ciesielski3 <r.ciesielski3@gmail.com>
2025-05-20 20:35:34 +0200

    feat: add question type

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,17 +1,9 @@
-import { ExampleBlock } from "./ExampleBlock";
-import { ExampleModal } from "./ExampleModal";
-import { InfoBox } from "./InfoBox";
-import { LogBox } from "./LogBox";
-import { SectionHeader } from "./SectionHeader";
-import { Tooltip } from "./Tooltip";
-import { useLogger } from "./useLogger";
+import { ExampleBlock } from './ExampleBlock';
+import { ExampleModal } from './ExampleModal';
+import { InfoBox } from './InfoBox';
+import { LogBox } from './LogBox';
+import { SectionHeader } from './SectionHeader';
+import { Tooltip } from './Tooltip';
+import { useLogger } from './useLogger';
 
-export {
-  ExampleBlock,
-  ExampleModal,
-  InfoBox,
-  LogBox,
-  SectionHeader,
-  Tooltip,
-  useLogger,
-};
+export { ExampleBlock, ExampleModal, InfoBox, LogBox, SectionHeader, Tooltip, useLogger };
