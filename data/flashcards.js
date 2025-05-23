fc3e543e2cace3313d65144fad38cc37d8f47c71
r.ciesielski3 <r.ciesielski3@gmail.com>
2025-05-23 20:10:08 +0200

    feat: add data for flashcard

---
+++ data/flashcards.js
@@ -0,0 +1,23 @@
+export const flashcards = [
+  {
+    question: 'What is a closure in JavaScript?',
+    answer: 'A closure gives access to an outer function’s scope from an inner function.'
+  },
+  {
+    question: 'What is the purpose of `useEffect` in React?',
+    answer: '`useEffect` is used to perform side effects in function components.'
+  },
+  {
+    question: 'What does `getServerSideProps` do in Next.js?',
+    answer: 'It fetches data on each request at the server level for a specific page.'
+  },
+  {
+    question: 'What is the difference between `null` and `undefined` in JS?',
+    answer:
+      '`undefined` means a variable has been declared but not assigned. `null` is an assignment value representing no value.'
+  },
+  {
+    question: 'Why should you use keys in React lists?',
+    answer: 'Keys help React identify which items changed, added, or removed.'
+  }
+];
